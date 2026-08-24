db47567a76066dacdc8b13c98a13b41600da063d
thiagotorres4006-creator <thiagotorres4006@gmail.com>
2026-08-24 10:22:02 -0300

    Add files via upload

---
+++ src/main/java/br/senac/dsw/exemplo_spring_boot/ExemploSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.senac.dsw.exemplo_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/dsw/exemplo_spring_boot/ExemploSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.dsw.exemplo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
